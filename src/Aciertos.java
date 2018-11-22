0dcaac33c9cbd16bd94bb6619e3816dfaf40b7ef
Adrian Flores Becerra <adrian.flores.0789@gmail.com>
2018-11-22 12:15:50 +0100

    upload

--- src/Aciertos.java
+++ src/Aciertos.java
@@ -2,7 +2,7 @@
 	import java.util.Scanner;
 
 	public class Aciertos {
-
+ssd
 	    public static void main(String[] args) {
 	        int n = 0,cont = 0;
 	        int x = (int) (500 *Math.random());
